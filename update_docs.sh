e49f868987cfd77ffe9860477d9a590c5da974d9
Gitlab-CI <gitlab-ci@scontain.com>
2022-11-03 10:23:09 +0000

    Deployed f8c74f58 with MkDocs version: 1.2.1

--- update_docs.sh
+++ update_docs.sh
@@ -1,4 +1,4 @@
-export ORIG=/Users/christoffetzer/GIT/Scontain/K8sOperators/docs
+export ORIG=/Users/christoffetzer/GIT/christoffetzer/K8sOperators/docs
 cp $ORIG/1_scone_operator.md .
 cp $ORIG/2_operator_installation.md .
 cp $ORIG/3_resource_definitions.md .
