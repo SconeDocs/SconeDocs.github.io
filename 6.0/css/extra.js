a4b0bb103ac2b110e0492deb359845fe775ed5c8
Gitlab-CI <gitlab-ci@scontain.com>
2026-08-04 13:54:24 +0000

    Update documentation

--- 6.0/css/extra.js
+++ 6.0/css/extra.js
@@ -1,27 +1,4 @@
 (function (document) {
-    function clipboard_init() {
-        var charts = document.querySelectorAll("div.codehilite"),
-            arr = [],
-            i, j, maxItem, pre, btn, el;
-
-        // Make sure we are dealing with an array
-        for(i = 0, maxItem = charts.length; i < maxItem; i++) arr.push(charts[i]);
-
-        // Find the UML source element and get the text
-        for (i = 0, maxItem = arr.length; i < maxItem; i++) {
-            el = arr[i];
-            pre = el.childNodes[0];
-
-            pre.id = "hl_code" + i.toString();
-            btn = document.createElement('button');
-            btn.appendChild(document.createTextNode('copy'));
-            btn.setAttribute("class", "btn");
-            btn.setAttribute("data-clipboard-target", "#hl_code" + i.toString());
-            el.insertBefore(btn, pre);
-        }
-        new Clipboard('.btn');
-    };
-
     function version_selector_init() {
         document.addEventListener("click", function (event) {
             var link = event.target.closest(".md-version__link"),
@@ -75,7 +52,7 @@
         }
     }
 
-    function keep_version_selector_visible_on_scroll() {
+    function bind_version_selector_refresh() {
         move_version_selector_to_active_header_topic();
         if (!window.addEventListener) {
             return;
@@ -83,22 +60,26 @@
 
         window.addEventListener("scroll", move_version_selector_to_active_header_topic, { passive: true });
         window.addEventListener("resize", move_version_selector_to_active_header_topic);
+
+        if (typeof document$ !== "undefined" && document$.subscribe) {
+            document$.subscribe(move_version_selector_to_active_header_topic);
+        }
     }
 
     function onReady(fn) {
         if (document.addEventListener) {
-            document.addEventListener('DOMContentLoaded', fn);
+            document.addEventListener("DOMContentLoaded", fn);
         } else {
-            document.attachEvent('onreadystatechange', function() {
-                if (document.readyState === 'interactive')
+            document.attachEvent("onreadystatechange", function() {
+                if (document.readyState === "interactive") {
                     fn();
+                }
             });
         }
     }
 
-    onReady(function(){
-        clipboard_init();
+    onReady(function () {
         version_selector_init();
-        keep_version_selector_visible_on_scroll();
+        bind_version_selector_refresh();
     });
 })(document);
